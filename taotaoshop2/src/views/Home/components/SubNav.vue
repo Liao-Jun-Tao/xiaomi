1258468259f96f838129479518d26b6f62d2e5da
廖骏涛 <1823152337@qq.com>
2024-10-28 10:33:08 +0800

    refactor(Home): 重构小导航栏组件
    - 修改数据结构：将嵌套的数据结构简化为单层数组
    - 优化模板：使用 grid 布局替代原有的 flex 布局
    - 添加滚动条功能：使用 vueuse 的 useScroll 钩子实现自定义滚动条
    -调整样式：优化样式，隐藏默认滚动条，添加自定义滚动条样式

--- taotaoshop2/src/views/Home/components/SubNav.vue
+++ taotaoshop2/src/views/Home/components/SubNav.vue
@@ -1,52 +1,98 @@
 <template>
-	<div class="subnav">
-		<van-swipe class="my-swipe" indicator-color="white">
-			<van-swipe-item>
-				<template v-for="items in subnav" :key="items.id">
-					<div class="wrapper">
-						<div class="content" v-for="item in items.data1">
-							<img class="photo" :src="item.imgSrc" alt="" />
-							<p class="title">{{ item.title }}</p>
-						</div>
-					</div>
-				</template>
-			</van-swipe-item>
-			<van-swipe-item>
-				<template v-for="items in subnav" :key="items.id">
-					<div class="wrapper">
-						<div class="content" v-for="item in items.data2">
-							<img class="photo" :src="item.imgSrc" alt="" />
-							<p class="title">{{ item.title }}</p>
-						</div>
-					</div>
-				</template>
-			</van-swipe-item>
-		</van-swipe>
-	</div>
+  <div ref="el" class="navBar_container" @scroll="handleScroll">
+    <div class="navBar_content">
+      <div v-for="item in subNav" :key="item.id" class="navBar_item">
+        <img :src="item.imgSrc" class="img_item">
+      </div>
+    </div>
+  </div>
+  <!--    自定义滚动条-->
+  <div class="scroll-wrapper" :style="{width: wrapperScrollWidth + 'px'}">
+    <div :style="offset" class="scroll-content" >
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { onMounted, computed, ref } from 'vue';
-import { useHomeStore } from '@/store/home.js';
+import {computed, onMounted, useTemplateRef} from 'vue';
+import {useScroll} from '@vueuse/core'
+import {useHomeStore} from '@/store/home.js';
+
+const el = useTemplateRef('el');
+
 const homeStore = useHomeStore();
-const subnav = computed(() => homeStore.subnav);
+const subNav = computed(() => homeStore.subnav);
+
+const {x} = useScroll(el, {behavior: 'smooth'})
+
+const wrapperScrollWidth = 750 / 10
+const contentScrollWidth = 750 / 10 * (1 / 3)
+
+const offset = computed(() => {
+  return {
+    left: `${x.value / 375  * (wrapperScrollWidth - contentScrollWidth)}px`,
+    width: contentScrollWidth + "px"
+  }
+})
+
+const handleScroll = () => {
+  // console.log(x.value)
+  console.log(offset.value.left)
+}
+
 onMounted(async () => {
-	await homeStore.getSubnav();
+  await homeStore.getSubnav();
 });
 </script>
 
 <style lang="stylus" scoped>
-.wrapper{
-   display: flex;
-   flex-wrap: wrap;
-  justify-content center
+
+//隐藏滚动条
+::-webkit-scrollbar {
+  display: none;
 }
-.content{
-   margin 0 10px
-   text-align: center
+
+.navBar_container {
+  width: 375px;
+  overflow: auto;
 }
-.photo{
-   width 50px
-   height 50px
+
+.navBar_content {
+  width: 750px;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(10, 1fr);
+  grid-auto-flow: column;
 }
+
+.navBar_item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.img_item {
+  width: 50px; /* 图像宽度 */
+  height: 50px; /* 图像高度 */
+}
+
+.scroll-wrapper {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+
+  height: 3px;
+  background-color: #f8f8f8;
+  border-radius: 5px;
+}
+
+.scroll-content {
+  height: 3px;
+  position: absolute;
+  border-radius: 5px;
+  background-color: #888888;
+}
+
+
 </style>
